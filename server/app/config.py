12436a6d1783b411d72bcd1b57dc355469b41cb7
Claus Gerull <c.gerull@nl.ccv.eu>
2019-12-19 15:01:55 +0100

    Add secret value.

--- server/app/config.py
+++ server/app/config.py
@@ -3,9 +3,7 @@ import os
 
 class Config(object):
     """Read and set configuration values"""
-    SECRET_KEY = os.environ.get(
-        'SECRET_KEY') or get_secret_key() or 'Only_the_default_secret_key'
 
-    def get_secret_key(self):
-        with open('/run/secrets/my_secret_key', 'r') as f:
-            return f.read()
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Only_the_default_secret_key'
+
+
--- server/app/routes.py
+++ server/app/routes.py
@@ -6,6 +6,11 @@ from flask import (request, jsonify, render_template, redirect,
                    url_for, flash)
 from datetime import datetime
 import socket
+import os
+
+# Modules constants
+secret_file = '/run/secrets/my_secret_key'
+
 
 #
 # HTML page
@@ -19,12 +24,26 @@ def index():
 
 def build_response_data():
     """
-    Build a dictionary with timestamp, server ip, server name and requester ip.
+    Build a dictionary with timestamp, server ip,
+    server name, secret and requester ip.
     """
     localhost = socket.gethostname()
     return {
         'now': datetime.now().isoformat(sep=' '),
         'local_ip': socket.gethostbyname(localhost),
         'container_name': localhost,
+        'secret': get_secret_key(),
         'remote_ip': request.remote_addr
     }
+
+
+def get_secret_key():
+        secret = ''
+        try:
+            f = open(secret_file, 'r')
+            secret = f.read()
+        except:
+            # no file, just return empty string
+            secret = os.environ.get('SECRET_KEY') or 'Only_the_default_secret_key'
+        
+        return secret
